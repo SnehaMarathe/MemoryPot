1a06a6eba6bae6d4722e1772c973411e9e0586ca
SnehaMarathe <doit13580@gmail.com>
2026-01-19 15:11:34 +0530

    Update MainActivity.kt

--- app/src/main/java/com/memorypot/MainActivity.kt
+++ app/src/main/java/com/memorypot/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.memorypot
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.memorypot.ui.AppRoot
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.memorypot.di.AppContainer
+import com.memorypot.di.LocalAppContainer
+import com.memorypot.ui.navigation.AppNav
+import com.memorypot.ui.theme.MemoryPotTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    AppRoot()
+            val appContainer = remember { AppContainer(this) }
+            val cameraPermissionState =
+                rememberPermissionState(Manifest.permission.CAMERA)
+
+            CompositionLocalProvider(
+                LocalAppContainer provides appContainer
+            ) {
+                MemoryPotTheme(useDynamicColor = false) {
+
+                    if (cameraPermissionState.status.isGranted) {
+                        AppNav()
+                    } else {
+                        cameraPermissionState.launchPermissionRequest()
+                    }
+
                 }
             }
         }
